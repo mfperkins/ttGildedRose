b9f84dd49135a2bad0db8b2386eff59b4d8e7d90
Matthew Perkins <mfperkins@gmail.com>
2016-10-19 15:23:44 +0100

    9) Got a working solution for conjure now starting refactor ...

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,11 @@
-describe("Gilded Rose update_quality", function() {
+describe("GildedRose", function() {
 
   var bread = new Item("bread", 10, 10);
   var oldBread = new Item("bread", 0, 4);
   var agedBrie = new Item("Aged Brie", 10, 20);
-  var sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
+  var sulfuras = new Item("Sulfuras, Hand of Ragnaros", 3, 10);
   var backstage = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10);
+  var conjured = new Item("Conjured", 5, 10)
 
   describe("Normal bread ...", function (){
 
@@ -101,12 +102,39 @@ describe("Gilded Rose update_quality", function() {
     it("quality should = 0 if sell_in < 0 days", function() {
       update_quality();
       update_quality();
-      console.log(items[0].sell_in);
       expect(items[0].quality).toEqual(0);
     });
 
-
   });
 
+  describe("Conjured ...", function() {
+
+    beforeEach(function(){
+      items = [ conjured ];
+      update_quality();
+    });
+
+    it("quality should reduce by two", function () {
+      update_quality();
+      expect(items[0].quality).toEqual(6);
+    });
+  });
+
+  describe("Check item ...", function () {
+
+    spyOn(update_quality, 'checkItem').and.callThrough();
+
+    beforeEach(function() {
+        items = [ bread, agedBrie ];
+        first_check = checkItem(item[0]);
+        second_check = checkItem(item[1]);
+    });
+
+    it('should detect "normal" item for bread and "brie" for "Aged Brie"', function() {
+      expect(first_check).toEqual("regular");
+      expect(second_check).toEqual("agedBrie");
+    });
+
+  });
 
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -8,7 +8,9 @@ var items = [];
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    item = items[i]
+    checkItem(item.name);
+    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && items[i].name != 'Conjured') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1;
@@ -17,6 +19,11 @@ function update_quality() {
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1;
+        if (items[i].name == 'Conjured') {
+          if (items[i].quality < 50) {
+              items[i].quality = items[i].quality - 3;
+            }
+          }
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
@@ -52,4 +59,27 @@ function update_quality() {
       }
     }
   }
+}
+
+function checkItem(item) {
+  switch(item) {
+    case "Aged Brie":
+      console.log("brie");
+      break;
+    case "Sulfuras, Hand of Rangaros":
+      console.log("sulfuras");
+      break;
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      console.log("backstage");
+      break;
+    case "Conjured":
+      console.log("conjured");
+    break;
+    default:
+      console.log("normal");
+  }
+}
+
+function normalUpdate(item) {
+
 }
