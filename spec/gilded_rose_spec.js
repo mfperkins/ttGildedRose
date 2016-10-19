87ffefea60043d7c196e0208511c19e94a2d41c4
Matthew Perkins <mfperkins@gmail.com>
2016-10-19 11:01:05 +0100

    4) Added first test for new and old bread

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,35 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  var bread = new Item("bread", 10, 10);
+  var oldBread = new Item("bread", 0, 4);
+
+  describe("For normal bread it ...", function (){
+
+    it("should reduce quality and sell_in by 1", function() {
+      items = [ bread ];
+      update_quality();
+      expect(items[0].sell_in).toEqual(9);
+      expect(items[0].quality).toEqual(9);
+    });
+
+  });
+
+  describe("For old bread it ...", function (){
+
+    it("should reduce quality by 2", function() {
+      items = [ oldBread ];
+      update_quality();
+      expect(items[0].quality).toEqual(2);
+    });
+
+    it("should not let quality drop below 0", function() {
+      items = [ oldBread ];
+      update_quality();
+      update_quality();
+      update_quality();
+      expect(items[0].quality).toEqual(0);
+    });
+
   });
 
 });
